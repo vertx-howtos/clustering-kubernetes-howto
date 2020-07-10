a7eae66e814ca2f08f0ec32b8c3ee21eb96b977c
Thomas Segismont <tsegismo@redhat.com>
2020-07-10 17:09:40 +0200

    Gradle build files

---
+++ frontend/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+  java
+  application
+  id("com.google.cloud.tools.jib") version "2.4.0"
+}
+
+repositories {
+  mavenCentral()
+}
+
+val vertxVersion = "4.0.0-milestone5"
+val verticle = "io.vertx.howtos.cluster.FrontendVerticle"
+
+dependencies {
+  implementation("io.vertx:vertx-web:${vertxVersion}")
+  implementation("io.vertx:vertx-infinispan:${vertxVersion}")
+  implementation("io.vertx:vertx-health-check:${vertxVersion}")
+  implementation("ch.qos.logback:logback-classic:1.2.3")
+}
+
+application {
+  mainClassName = verticle
+}
+
+jib {
+  to {
+    image = "clustering-kubernetes/frontend"
+  }
+  will container {
+    mainClass = "io.vertx.core.Launcher"
+    args = listOf("run", verticle, "-cluster")
+    ports = listOf("8080", "7800")
+  }
+}
+
+tasks.wrapper {
+  gradleVersion = "5.2"
+}
